e1474e76f68f4ff398f08e96110c10acb3ed6778
Bernd Beuster <bernd.beuster@googlemail.com>
2014-03-05 23:17:10 +0100

    Removed union. Changed RAM adress from _st0 to st0.

--- rtl/j1.sv
+++ rtl/j1.sv
@@ -6,92 +6,82 @@
  *     jamesb@willowgarage.com
  */
 
-module j1(input         sys_clk_i, // main clock
-	  input         sys_rst_i, // reset
-	  input  [15:0] io_din,    // io data in
-	  output        io_rd,     // io read
-	  output        io_wr,     // io write
-	  output [15:0] io_addr,   // io address
-	  output [15:0] io_dout);  // io data out
+module j1(input               sys_clk_i, // main clock
+	  input               sys_rst_i, // reset
+	  input        [15:0] io_din,    // io data in
+	  output logic        io_rd,     // io read
+	  output logic        io_wr,     // io write
+	  output logic [15:0] io_addr,   // io address
+	  output logic [15:0] io_dout);  // io data out
+
+   typedef enum logic [2:0] {TAG_UBRANCH,TAG_ZBRANCH,TAG_CALL,TAG_ALU} tag_t;
 
    typedef enum logic [3:0] {OP_T,OP_N,OP_T_PLUS_N,OP_T_AND_N,
 			     OP_T_IOR_N,OP_T_XOR_N,OP_INV_T,OP_N_EQ_T,
 			     OP_N_LS_T,OP_N_RSHIFT_T,OP_T_MINUS_1,OP_R,
 			     OP_AT,OP_N_LSHIFT_T,OP_DEPTH,OP_N_ULS_T} op_t;
 
-   typedef enum logic [2:0] {INSN_UBRANCH,INSN_ZBRANCH,
-			     INSN_CALL,INSN_ALU} jmp_call_t;
+   typedef struct packed {
+      logic        tag;
+      logic [14:0] immediate;
+   } lit_t;
 
-   typedef union packed {
-      struct packed {
-	 logic        tag;
-	 logic [14:0] immediate;
-      } lit;
-      
-      struct packed {
-	 jmp_call_t   tag;
-	 logic [12:0] address;
-      } ubranch;
+   typedef struct packed {
+      tag_t        tag;
+      logic [12:0] address;
+   } branch_t;
 
-      struct packed {
-	 jmp_call_t   tag;
-	 logic [12:0] address;
-      } zbranch;
+   typedef struct packed {
+      tag_t              tag;
+      logic              r_to_pc;
+      op_t               alu_op;
+      logic              t_to_n;
+      logic              t_to_r;
+      logic              n_to_mem;
+      logic              reserved;
+      logic signed [1:0] rstack;
+      logic signed [1:0] dstack;
+   } alu_t;
 
-      struct packed {
-	 jmp_call_t   tag;
-	 logic [12:0] address;
-      } call;
+   logic [15:0] insn;      // instruction
+   logic [12:0] _pc,pc;    // processor counter
+   logic [12:0] pc_plus_1; // processor counter + 1
+   logic        io_sel;    // I/O select
 
-      struct packed {
-	 jmp_call_t         tag;
-	 logic              r_to_pc;
-	 op_t               alu_op;
-	 logic              t_to_n;
-	 logic              t_to_r;
-	 logic              n_to_mem;
-	 logic              reserved;
-	 logic signed [1:0] rstack;
-	 logic signed [1:0] dstack;
-      } alu;
-   } insn_t;
+   /* select instruction types */
+   logic is_lit,is_ubranch,is_zbranch,is_call,is_alu;
 
-   var insn_t  insn;
-   wire [15:0] immediate = {1'b0,insn.lit.immediate};
+   /* RAM */
+   wire  [15:0] ramrd;  // RAM read data
+   logic        _ramWE; // RAM write enable
 
-   wire [15:0] ramrd;
-   logic       io_sel;
+   /* data stack */
+   logic        [15:0] dstack[32]; // data stack memory
+   logic        [4:0]  _dsp,dsp;   // data stack pointer
+   logic        [15:0] _st0,st0;   // top of data stack
+   logic        [15:0] st1;        // next of data stack
+   logic               _dstkW;     // data stack write
 
-   logic [4:0]  _dsp,dsp;
-   logic [15:0] _st0,st0;
-   wire [15:0]  st1;
-   wire         _dstkW;     // D stack write
+   /* return stack */
+   logic        [15:0] rstack[32]; // return stack memory
+   logic        [4:0]  _rsp,rsp;   // return stack pointer
+   logic        [15:0]  rst0;      // top of return stack
+   logic        [15:0] _rstkD;     // return stack data
+   logic               _rstkW;     // return stack write
 
-   logic [12:0] _pc,pc;
-   logic [4:0]  _rsp,rsp;
-   wire [15:0]  rst0;
-   logic        _rstkW;     // R stack write
-   logic [15:0] _rstkD;
-   wire _ramWE;             // RAM write enable
+   dpram8kx16 dpram(.clock(sys_clk_i),
 
-   wire [12:0] pc_plus_1;
-   assign pc_plus_1 = pc + 13'd1;
-
-   /* Restricted from 16 Kwords to 8 Kwords because of limited memory resources of the Altera Cyclone II.*/
-   dpram8kx16 dpram(.address_a(_pc),
-		    .address_b(_st0[13:1]),
-		    .clock(sys_clk_i),
-		    .data_a(16'b0),
-		    .data_b(st1),
+		    .address_a(_pc),
+		    .data_a(16'h0),
 		    .wren_a(1'b0),
-		    .wren_b(_ramWE & (_st0[15:14] == 0)), // TODO: extra signal
 		    .q_a(insn),
+
+		    .address_b(/*_st0[13:1]*/st0[13:1]),
+		    .data_b(st1),
+		    .wren_b(_ramWE),
 		    .q_b(ramrd));
 
-
-   logic [15:0] dstack[32];
-   logic [15:0] rstack[32];
-
+   /* data and return stack */
    always_ff @(posedge sys_clk_i)
      begin
 	if (_dstkW)
@@ -101,29 +91,49 @@ module j1(input         sys_clk_i, // main clock
 	  rstack[_rsp] = _rstkD;
      end
 
-   assign st1  = dstack[dsp];
-   assign rst0 = rstack[rsp];
+   always_comb
+     begin
+	st1  = dstack[dsp];
+	rst0 = rstack[rsp];
+     end
 
-   always_comb io_sel = (st0[15:14] != 2'b00); // RAM:00000H...3FFFH I/O:4000H...FFFFH
+   /* select instruction types */
+   always_comb
+     begin
+	/* Because unions are not supported by the Quartus II software we have to work around it. */
+	var lit_t    lit_instr;
+	var branch_t bra_instr;
 
-   wire is_lit     = (insn.lit.tag);
-   wire is_ubranch = (insn.ubranch.tag == INSN_UBRANCH);
-   wire is_zbranch = (insn.zbranch.tag == INSN_ZBRANCH);
-   wire is_call    = (insn.call.tag    == INSN_CALL);
-   wire is_alu     = (insn.alu.tag     == INSN_ALU);
+	lit_instr  = insn;
+	bra_instr  = insn;
+	is_lit     = lit_instr.tag;
+	is_ubranch = (bra_instr.tag == TAG_UBRANCH);
+	is_zbranch = (bra_instr.tag == TAG_ZBRANCH);
+	is_call    = (bra_instr.tag == TAG_CALL);
+	is_alu     = (bra_instr.tag == TAG_ALU);
+     end
 
+   /* calculate next TOS value */
    always_comb
      if (is_lit)
-       _st0 = immediate;
+       begin
+	  var lit_t instr;
+
+	  instr = insn;
+	  _st0  = {1'b0,instr.immediate};
+       end
      else
        begin
-	  var op_t op;
+	  var op_t  op;
+	  var alu_t instr;
+
+	  instr = insn;
 
 	  unique case (1'b1)
 	    is_ubranch:  op = OP_T;
 	    is_zbranch:  op = OP_N;
 	    is_call   :  op = OP_T;
-	    is_alu    :  op = op_t'(insn.alu.alu_op);
+	    is_alu    :  op = op_t'(instr.alu_op);
 	    default      op = op_t'('x);
 	  endcase
 
@@ -144,71 +154,100 @@ module j1(input         sys_clk_i, // main clock
             OP_N_LSHIFT_T: _st0 = st1 << st0[3:0];
             OP_DEPTH     : _st0 = {rsp,3'b000,dsp};
             OP_N_ULS_T   : _st0 = {16{(st1 < st0)}};
-            default        _st0 = 16'hxxxx;
+            default        _st0 = 16'hx;
 	  endcase
        end
 
-   assign io_rd   = (is_alu && (insn.alu.alu_op == OP_AT) && io_sel);
-   assign io_wr   = _ramWE;
-   assign io_addr = st0;
-   assign io_dout = st1;
+   /* I/O and RAM control */
+   always_comb
+     begin
+	var alu_t instr;
+	logic     wr_en;
 
-   assign _ramWE = is_alu & insn.alu.n_to_mem;
-   assign _dstkW = is_lit | (is_alu & insn.alu.t_to_n);
+	instr   = insn;
+	wr_en   = is_alu & instr.n_to_mem;
+	io_sel  = (st0[15:14] != 2'b00); // I/O:4000H...FFFFH
+	io_rd   = (is_alu && (instr.alu_op == OP_AT) && io_sel);
+	io_wr   = wr_en & io_sel;
+	io_addr = st0 >> 1; // changed from original design
+	io_dout = st1;
 
-   wire signed [4:0] dd = insn.alu.dstack;  // D stack delta
-   wire signed [4:0] rd = insn.alu.rstack;  // R stack delta
+	//_ramWE = wr_en && (_st0[15:14] == 2'b00); // RAM:00000H...3FFFH
+	_ramWE = wr_en && !io_sel; // RAM:00000H...3FFFH
+	_dstkW = is_lit | (is_alu & instr.t_to_n);
+     end
+
+   always_comb pc_plus_1 = pc + 13'd1;
 
    always_comb
+     /* literals */
      if (is_lit)
        begin
 	  _dsp   = dsp + 5'd1;
 	  _rsp   = rsp;
 	  _rstkW = 0;
-	  _rstkD = _pc;
+	  _rstkD = 16'hx; // don't care
        end
+   /* ALU operations */
      else if (is_alu)
        begin
+	  var alu_t          instr;
+	  logic signed [4:0] dd,rd; // stack delta
+
+	  instr  = insn;
+	  dd     = instr.dstack;
+	  rd     = instr.rstack;
 	  _dsp   = dsp + dd;
-	  _rsp   = {$signed(rsp) + $signed(rd)};
-	  _rstkW = insn.alu.t_to_r;
+	  _rsp   = rsp + rd;
+	  _rstkW = instr.t_to_r;
 	  _rstkD = st0;
        end
-     else // jump/call
+     else
+       /* jump/call */
        begin
-	  // predicated jump is like DROP
+	  /* predicated jump is like DROP */
 	  if (is_zbranch)
             _dsp = dsp - 5'd1;
 	  else
             _dsp = dsp;
 
 	  if (is_call)
-	    begin // call
+	    /* call */
+	    begin
 	       _rsp   = rsp + 5'd1;
-	       _rstkW = 1;
-	       //_rstkD = {pc_plus_1[14:0],1'b0};
+	       _rstkW = 1'b1;
 	       _rstkD = pc_plus_1 << 1;
 	    end
 	  else
+	    /* */
 	    begin
 	       _rsp   = rsp;
-	       _rstkW = 0;
-	       _rstkD = _pc;
+	       _rstkW = 1'b0;
+	       _rstkD = 16'hx; // don't care
 	    end
        end
 
+   /* control PC */
    always_comb
-     if (sys_rst_i)
-       _pc = pc;
-     else
-       if (is_ubranch || (is_zbranch && (st0 == 16'h0000)) || is_call)
-         _pc = insn.zbranch.address;
-       else if (is_alu && insn.alu.r_to_pc)
-         //_pc = {1'b0,rst0[15:1]};
-         _pc = rst0 >> 1;
-       else
-         _pc = pc_plus_1;
+     begin
+	var branch_t bra_instr;
+	var alu_t    alu_instr;
 
+	bra_instr = insn;
+	alu_instr = insn;
+
+	if (sys_rst_i)
+	  _pc = pc;
+	else
+	  if (is_ubranch || (is_zbranch && (st0 == 16'h0)) || is_call)
+            _pc = bra_instr.address;
+	  else if (is_alu && alu_instr.r_to_pc)
+            _pc = rst0 >> 1;
+	  else
+            _pc = pc_plus_1;
+     end
+
+   /* update PC and stacks */
    always_ff @(posedge sys_clk_i)
      if (sys_rst_i)
        begin
