67fb39c15e64126b9a3fc3bce181c10772c43a1d
pbing <bernd.beuster@googlemail.com>
2014-03-08 11:00:15 +0100

    Save a cell with '0do' instead of '0 do'.

--- src/application.fs
+++ src/application.fs
@@ -5,8 +5,8 @@ include io-addr.fs
 include hex-display.fs
 include led.fs
 
-: wait      ( u -- ) d# 0 do loop ;
-: wait-1000 ( u -- ) d# 0 do d# 1000 wait loop ;
+: wait      ( u -- ) d# 0do loop ;
+: wait-1000 ( u -- ) d# 0do d# 1000 wait loop ;
 
 : main ( --)
   d# 1
