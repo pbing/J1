08955ae770ca6826b6d0d46fff0abb1420bff69f
Bernd Beuster <bernd.beuster@gmail.com>
2015-05-15 09:54:31 +0200

    Use dash instead of underline in names.

--- src/io-addr.fs
+++ src/io-addr.fs
@@ -1,15 +1,15 @@
 \ I/O addresses
 
 \ LED
-h# 4000 constant io_ledg
-h# 4002 constant io_ledr
+h# 4000 constant io-ledg
+h# 4002 constant io-ledr
 
 \ HEX display
-h# 4010 constant io_hex0
-h# 4012 constant io_hex1
-h# 4014 constant io_hex2
-h# 4016 constant io_hex3
+h# 4010 constant io-hex0
+h# 4012 constant io-hex1
+h# 4014 constant io-hex2
+h# 4016 constant io-hex3
 
 \ keys and switches
-h# 4020 constant io_key 
-h# 4022 constant io_sw  
+h# 4020 constant io-key 
+h# 4022 constant io-sw  
